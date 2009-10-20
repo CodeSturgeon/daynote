7f0be735805f66ac3630edd8fa2f071055ffb9e2
Nick Fisher <fish@FishBook.local>
2009-10-20 13:25:54 -0400

    Tweaking to get JS and PY working happily

--- titanium/daynote/Resources/mod.py
+++ titanium/daynote/Resources/mod.py
@@ -1,4 +1,22 @@
 import couchdb
 
+server = couchdb.Server('http://localhost:5984/')
+db = server['relaxer']
+
+def save():
+    Titanium.API.debug('--save--')
+
+def pyonclick(evt):
+    Titanium.API.debug('click (%s)'%evt.target.id)
+    if evt.target.id == 'save_and_new':
+        js_lookup(jQuery('form'),0).reset()
+
 def go():
-    window.alert('hi there!')
+    Titanium.API.debug('loading')
+    jQuery('textarea#detail').attr('value', db.name)
+    jQuery('textarea#detail').click(pyonclick)
+    jQuery('button#save').click(pyonclick)
+    jQuery('button#save_and_new').click(pyonclick)
+    jQuery('form').attr('action','javascript:save()')
+
+jQuery(document).ready(js_callback(go))
